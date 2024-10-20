a45f48bc097ba1b10b33be8318feb8b85930aa22
Vallen Dsouza <123362088+Vallen328@users.noreply.github.com>
2024-10-21 00:25:49 +0530

    Commited web app project

---
+++ NLP Web App/app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request as req
+import requests
+from config import HF_API_KEY  # Import the API key
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def Index():
+    return render_template("index.html")
+
+@app.route("/Summarize", methods=["GET", "POST"])
+def Summarize():
+    if req.method == "POST":
+        API_URL = "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"
+        headers = {"Authorization": f"Bearer {HF_API_KEY}"}
+
+        data = req.form["data"]
+        maxL = int(req.form["maxL"])
+        minL = maxL // 4
+
+        def query(payload):
+            response = requests.post(API_URL, headers=headers, json=payload)
+            return response.json()
+
+        output = query({
+            "inputs": data,
+            "parameters": {"min_length": minL, "max_length": maxL},
+        })
+
+        if isinstance(output, list) and "summary_text" in output[0]:
+            summary = output[0]["summary_text"]
+        else:
+            summary = "Unable to generate summary. Please try again later."
+
+        return render_template("index.html", result=summary)
+    else:
+        return render_template("index.html")
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
